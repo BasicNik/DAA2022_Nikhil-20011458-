509f15b5b4c53be28261b0872fbceb570f9cf131
Nikhil Verma <90186321+BasicNik@users.noreply.github.com>
2022-06-05 22:59:12 +0530

    Update Q1.cpp

--- Practicals/Week_6/Q1.cpp
+++ Practicals/Week_6/Q1.cpp
@@ -1,50 +1,49 @@
-#include<iostream>
-#include<limits.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-void intersection(int arr1[],int n1,int arr2[],int n2)
+void DFS(int node,int destination,vector<int> &vis,vector<int> adj[],vector<int> &storedfs,int &flag)
 {
-    int maxi1=INT_MIN;
-    for (int i=0;i<n1;i++)
+    storedfs.push_back(node);
+    vis[node]=1;
+    for (auto it: adj[node])
     {
-        if (arr1[i]>maxi1)
-           maxi1=arr1[i];
+        if (!vis[it])
+        {
+            if (it==destination) flag=1;
+            DFS(it,destination,vis,adj,storedfs,flag);
+        }
+         
     }
-    int temp1[maxi1+1]={0};
-    for (int i=0;i<n1;i++)
-        temp1[arr1[i]]++;
-        
-    int maxi2=INT_MIN;
-    for (int i=0;i<n2;i++)
-    {
-        if (arr2[i]>maxi2)
-           maxi2=arr2[i];
-    }
-    int temp2[maxi2+1]={0};
-    for (int i=0;i<n2;i++)
-        temp2[arr2[i]]++;
-        
-    for (int i=0;i<n1;i++)
-    {
-        if (temp1[arr1[i]]!=0 && temp2[arr1[i]]!=0)
-           cout<<arr1[i]<<" ";
-    }   
 }
-
 int main()
 {
-    int n1;
-    cin>>n1;
-    int arr1[n1];
-    for (int i=0;i<n1;i++)
-      cin>>arr1[i];
-     
-    int n2;
-    cin>>n2;
-    int arr2[n2];
-    for (int i=0;i<n2;i++)
-      cin>>arr2[i];
-    
-    intersection(arr1,n1,arr2,n2);
+    int n,m;         //n=no of edges, m= no of lines of edges 
+    cout<<"enter n and m"<<endl;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    cout<<"enter the nodes between there is edge"<<endl;
+    for (int i=1;i<=m;i++)
+    {
+        int u,v;          //ex:- (2,3) is one line which mens there is a edge beetween 2 and 3
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int source,destination;
+    cout<<"Enter the source and Destination node"<<endl;
+    cin>>source>>destination;
+    vector<int> storedfs;
+    vector<int> vis(n+1,0);
+    int flag=0;
+    DFS(source,destination,vis,adj,storedfs,flag);
+    for (int i=0;i<storedfs.size();i++)
+       cout<<storedfs[i]<<" ";
+       cout<<endl;
+
+    if (flag)
+       cout<<"Yes Path exists"<<endl;
+    else
+       cout<<"No Such Path exists"<<endl;
+
     return 0;
 }
